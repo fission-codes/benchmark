6f9b60c7b41c8704aa934d59e43b9fc1fa8dc8c8
James Walker <walkah@walkah.net>
2021-02-08 14:37:11 -0500

    initial snowpack setup

---
+++ snowpack.config.js
@@ -0,0 +1,19 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
