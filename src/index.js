277b846595f13091f04429fca482ce7841af3f05
James Walker <walkah@walkah.net>
2021-02-11 14:54:08 -0500

    add webnative + basic auth/leave

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-// Main implementation code
+import * as wn from 'webnative'
+let fs;
+
+wn.setup.debug({ enabled: true })
+
+performance.mark('BEGIN_INITIALISE');
+wn.initialise({
+  permissions: {
+    app: {
+      name: "Performance",
+      creator: "Fission"
+    },
+  }
+}).then(async state => {
+  performance.mark('END_INITIALISE')
+  performance.measure('INITIALISE', 'BEGIN_INITIALISE');
+
+  switch (state.scenario) {
+    case wn.Scenario.NotAuthorised:
+      console.warn('not authorised')
+      showIds('auth')
+      hideIds('leave')
+      break;
+    case wn.Scenario.AuthCancelled:
+      console.warn('auth cancelled')
+      showIds('auth')
+      hideIds('leave')
+      break;
+    case wn.Scenario.AuthSucceeded:
+    case wn.Scenario.Continuation:
+      showIds('leave')
+      hideIds('auth')
+      fs = state.fs;
+    break;
+  }  
+
+  const auth = () => {
+    wn.redirectToLobby(state.permissions)
+  }
+
+  const leave = () => {
+    wn.leave()
+  }
+
+  document.getElementById('auth').onclick = auth;
+  document.getElementById('leave').onclick = leave;
+
+  renderMeasurements();
+}).catch(err => {
+  console.error(err)
+});
+
+const renderMeasurements = () => {
+  const measures = performance.getEntriesByType('measure');
+  measures.forEach(measureItem => {
+    console.log(`${measureItem.name}: ${measureItem.duration}`);
+  });
+}
+
+const showIds = (...ids) => {
+  ids.forEach(id => {
+    document.getElementById(id).style.display = 'flex'
+  })
+}
+
+const hideIds = (...ids) => {
+  ids.forEach(id => {
+    document.getElementById(id).style.display = 'none'
+  })
+}
