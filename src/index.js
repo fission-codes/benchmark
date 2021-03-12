8c9609987555649cead4daacc7ae0e6b0b2bad35
James Walker <walkah@walkah.net>
2021-03-12 17:54:33 -0500

    fix env selector

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ feather.replace();
 
 wn.setup.debug({ enabled: true });
 
-init();
+const env = sessionStorage.getItem('BENCHMARK_ENV')
+init(env);
 
 document.getElementById('environment').onchange = (e) => {
-  console.log('Changed env to: ', e.target.value)
-  init(e.target.value)
+  const env = e.target.value;
+  console.log(`Changed env to: ${env}`)
+  sessionStorage.setItem('BENCHMARK_ENV', env)
+  init(env)
 }
