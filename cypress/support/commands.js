3dab2a1b7d40897508776424870fa32f32456534
James Walker <walkah@walkah.net>
2021-10-27 12:35:22 -0400

    increase auth timeout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,23 +3,25 @@
  * We don't reset the browser for every test, so it will conditionally create a new account or approve.
  */
 Cypress.Commands.add("handleLobby", (email) => {
-  cy.url().should('contain', 'auth')
-  cy.get('button').first().then(($btn) => {
-    if ($btn.text() === 'Yes') {
-      $btn.click()
-    } else {
-      $btn.click()
-      cy.get('#email').type('james@fission.codes');
-      cy.get('#username').type('test-' + Date.now());
-      cy.contains('Get started').click();
-      cy.contains('Remind me later').click();
-      cy.contains('Yes').click();
-    }
-  })
+  cy.url().should("contain", "auth");
+  cy.get("button")
+    .first()
+    .then(($btn) => {
+      if ($btn.text() === "Yes") {
+        $btn.click();
+      } else {
+        $btn.click();
+        cy.get("#email").type("james@fission.codes");
+        cy.get("#username").type("test-" + Date.now());
+        cy.contains("Get started").click();
+        cy.contains("Remind me later", { timeout: 30000 }).click();
+        cy.contains("Yes").click();
+      }
+    });
 });
 
 Cypress.Commands.add("clearAuth", () => {
   sessionStorage.clear();
   indexedDB.deleteDatabase("localforge");
   indexedDB.deleteDatabase("keystore");
-})
+});
